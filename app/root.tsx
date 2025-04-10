898c1c67211cbbf665d7d0093264a68f5a1785aa
Jorin Vogel <hi@jorin.me>
2025-04-10 19:58:00 +0700

    demo shaper-react

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="pt-10 px-5">
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { ShaperDashboard } from "shaper-react";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +9,24 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  const getJwt = async (): Promise<string> => {
+    console.log('get jwt');
+    const res = await fetch("/api/jwt", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+    return res.json();
+  };
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Embedding Demo</h1>
+      <p>Dashboard will be embedded here:</p>
+      <ShaperDashboard
+        baseUrl="http://localhost:5454"
+        dashboardId="hktf9zy22rurvut8txg931m9"
+        getJwt={getJwt}
+      />
+    </div>
+  );
 }
