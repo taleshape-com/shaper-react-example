c4f2c0051f3d337d7aa9a8d5bd98e6cce95f5b32
Jorin Vogel <hi@jorin.me>
2025-04-13 17:03:07 +0700

    include demo db

--- app/env.ts
+++ app/env.ts
@@ -1,16 +1,9 @@
-const DASHBOARD_ID = process.env.DASHBOARD_ID;
-const API_KEY = process.env.API_KEY;
-const BASE_URL = process.env.BASE_URL || "http://localhost:5454";
-const VARIABLES = process.env.VARIABLES || "{}";
+const DASHBOARD_ID = process.env.DASHBOARD_ID ?? 'hktf9zy22rurvut8txg931m9';
+const API_KEY = process.env.API_KEY ?? 'shaperkey.dezrwe4wk1ib1ps1l800d21h.JXPttYgCVakWLZkVdIaT5p7wk0B2lBfZ';
+const BASE_URL = process.env.BASE_URL ?? "http://localhost:5454";
+const VARIABLES = process.env.VARIABLES ?? "{}";
 
 export function env() {
-  if (!DASHBOARD_ID) {
-    throw new Error("DASHBOARD_ID env var is required");
-  }
-  if (!API_KEY) {
-    throw new Error("API_KEY env var is required");
-  }
-
   return {
     DASHBOARD_ID,
     API_KEY,
